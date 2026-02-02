a998a3f48f1eaec7b88401d30894050792dda30a
IWantWater <calebkim920@gmail.com>
2026-02-01 21:44:30 -0800

    Deletion Test 1

--- Main.java
+++ Main.java
@@ -8,26 +8,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter size a for an a x a multiplication table: ");
-        int a;
-        if (sc.hasNextInt()) {
-            a = sc.nextInt();
-        } else {
-            System.out.println("Invalid input. Please enter a positive integer.");
-            sc.close();
-            return;
-        }
-
-        if (a <= 0) {
-            System.out.println("Please enter a positive integer greater than 0.");
-            sc.close();
-            return;
-        }
-
-        int max = a * a;
-        int cellWidth = String.valueOf(max).length() + 1;
-        int labelWidth = String.valueOf(a).length();
+        
 
         // Print header
         System.out.printf("%" + (labelWidth + 2) + "s", "");
